880cb5134b6c99a9f697ab490351d2a9bfa61d74
Sean Lees <sean@Seans-MacBook-Air.local>
2026-01-23 17:43:51 +0000

    chore: release v0.1.0

--- Formula/md.rb
+++ Formula/md.rb
@@ -1,12 +1,12 @@
 class Md < Formula
   desc "Beautiful terminal markdown viewer"
-  homepage "https://github.com/seanbreckenridge/markdown-display"
-  version "0.0.1"
+  homepage "https://github.com/seanmozeik/markdown-display"
+  version "0.1.0"
   license "MIT"
 
   # URL to bundled source (single JS file)
-  url "https://github.com/seanbreckenridge/markdown-display/releases/download/v#{version}/md-#{version}.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/seanmozeik/markdown-display/releases/download/v#{version}/md-#{version}.tar.gz"
+  sha256 "b69e3dc6a85fc91fe35d71963aff7862d1e15d5a1618878517c90ea1337a8ed9"
 
   depends_on "bun"
 
@@ -14,11 +14,14 @@ class Md < Formula
     # Install all bundled files to libexec
     libexec.install Dir["*"]
 
-    # Create shim that runs: bun /path/to/libexec/index.js
-    (bin/"md").write_env_script "bun", libexec/"index.js"
+    # Create wrapper script
+    (bin/"md").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["bun"].opt_bin}/bun" "#{libexec}/index.js" "$@"
+    EOS
   end
 
   test do
-    assert_match "md v#{version}", shell_output("#{bin}/md --version")
+    assert_match "md", shell_output("#{bin}/md --help")
   end
 end
